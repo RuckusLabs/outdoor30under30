cec180b85094aa48d3cb177d9bd3b039a5790370
Damian Makki <damian_makki@apple.com>
2023-06-22 15:45:28 -0500

    replace hover state with mouseEnter/mouseLeave functionality (to better support mobile styling)

--- src/routes/theList/index.js
+++ src/routes/theList/index.js
@@ -2,6 +2,15 @@ import style from "./index.css";
 import Data from '../../assets/data/2016-winners.json';
 
 const TheList = () => {
+
+  function handleMouseEnter(e) {
+    e.target.classList.add(`${style.active}`);
+  }
+
+  function handleMouseLeave(e) {
+    e.target.classList.remove(`${style.active}`);
+  }
+
   return (
     <>
       <h2>The List</h2>
@@ -9,7 +18,11 @@ const TheList = () => {
       <ul class={style.winnerList}>
         {
           Data.map((winner) => (
-            <li key={winner.id}>
+            <li
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              key={winner.id}
+            >
               <p class={style.winnerName}>{winner.name}</p>
               <p class={style.winnerCompany}>{winner.company ? winner.company : winner.title}</p>
               <div class={style.winnerProfileContainer}>
